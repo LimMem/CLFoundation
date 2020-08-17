115a7c82068c7a4ccf14e278bcc66238e7f4d984
qcl901028 <qcl901028@gmail.com>
2020-08-17 13:20:47 +0800

    修改版本号

--- CLFoundation/Classes/QCLFoundation.h
+++ CLFoundation/Classes/QCLFoundation.h
@@ -9,16 +9,16 @@
 #ifndef QCLFoundation_h
 #define QCLFoundation_h
 
-#import <CTMediator.h>
-#import <MBProgressHUD.h>
+#import <CTMediator/CTMediator.h>
+#import <MBProgressHUD/MBProgressHUD.h>
 #import <QMUIKit.h>
 #import <AFNetworking/AFURLSessionManager.h>
-#import <MJExtension.h>
-#import <YTKNetwork.h>
-#import <Masonry.h>
+#import <MJExtension/MJExtension.h>
+#import <YTKNetwork/YTKNetwork.h>
+#import <Masonry/Masonry.h>
 #import <ReactiveObjC/ReactiveObjC.h>
-#import <UIButton+WebCache.h>
-#import <UIImageView+WebCache.h>
+#import <SDWebImage/UIButton+WebCache.h>
+#import <SDWebImage/UIImageView+WebCache.h>
 
 
 #endif /* QCLFoundation_h */
